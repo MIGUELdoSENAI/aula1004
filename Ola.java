f4ddc9c629eee691491de336a20e82fdb6ca3b52
MIGUELdoSENAI <miguel.b.maciel@aluno.senai.br>
2025-04-10 16:34:59 -0300

    Add UI to Ola.java

--- Ola.java
+++ Ola.java
@@ -1,6 +1,7 @@
 public class Ola { 
     public static void main(String[] args) {
-        System.out.println("Boa Tarde a Todos!");
+        //System.out.println("Boa Tarde a Todos!");
+        JOptionsPane.ShowMessageDialog(parentComponent, "Boa Tarde a Todos!", "Ola");
     }
     
 } 
